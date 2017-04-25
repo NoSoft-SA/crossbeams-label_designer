053c242baed91a80b26da04b471331ebc523bd95
Violet <heilahendriks1@gmail.com>
2017-04-25 04:24:17 -0700

    Add Label design content [25/04/17]

--- lib/crossbeams/label_designer/page.rb
+++ lib/crossbeams/label_designer/page.rb
@@ -22,58 +22,24 @@ module Crossbeams
       #    str = Erubi::Engine.new(File.load(...))
       #    eval(str.src)
       def render
-        <<-EOS.freeze
-        <div id="label_designer">
-          <h1>An embedded area</h1>
-          <p>This tests the loading of a few "pages"</p>
-          <p>To test, pass one of the following values for the file to load:
-          <ul>
-          <li>leave blank - simulates loading a new canvas</li>
-          <li><em>blue</em> - simulates loading file 1</li>
-          <li><em>green</em> - simulates loading file 2</li>
-          </ul>
-          <div id="workspace">
-            <span class="#{@file_to_load}">Loaded file: <strong>#{@file_to_load}</strong></span>
-            <p>This is clickable. The JSON load path is set to <em>#{json_load_path}</em></p>
-          </div>
-        </div>
-        EOS
+        File.read(File.join(File.dirname(__FILE__), 'assets/_label_design.html'))
       end
 
       def javascript
+        file = File.join(File.dirname(__FILE__), 'assets/label_design.js')
         <<-EOS.freeze
         <script type="text/javascript">
-
-          var label_variables = #{Config.config.label_variables.to_json}; // Load JSON vars
-          var file_to_load = '#{@file_to_load}';
-
-          var theBox = document.getElementById('workspace');
-          theBox.addEventListener('click', function (event) {
-            alert('I was clicked');
-          })
+          #{File.read(file)}
         </script>
         EOS
       end
 
       def css
+        file = File.join(File.dirname(__FILE__), 'assets/label_design.css')
+        #File.read(file)
         <<-EOS.freeze
         <style>
-        #label_designer {
-          padding: 0 1em;
-        }
-        #workspace {
-          padding: 2em;
-          border: thin solid #999;
-        }
-        .new {
-          color:black;
-        }
-        .blue {
-          color: purple;
-        }
-        .green {
-          color: green;
-        }
+          #{File.read(file)}
         </style>
         EOS
       end
