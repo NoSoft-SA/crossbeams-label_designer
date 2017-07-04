88b3ec2cee9b08377d0b5c0f03652a3e685d58e4
Violet <heilahendriks1@gmail.com>
2017-07-04 03:24:44 -0700

    label config

--- lib/crossbeams/label_designer/config.rb
+++ lib/crossbeams/label_designer/config.rb
@@ -3,17 +3,22 @@ module Crossbeams
     class Config
       extend Dry::Configurable
 
-      setting :json_load_path # Expect Konva JSON to load from
-      setting :json_create_path # Expect name
-      setting :json_save_path # return image, Konva json & xml all as JSON
-
-      setting :label_sizes, [{ 'name' => 'a4', 'mm_size' => '71x54' },
-                             { 'name' => 'a5', 'mm_size' => '35x21' }]
+      setting :label_sizes, [{ 'name' => 'a4', 'mm_size' => {'width' => '71', 'height' => '54'} },
+                             { 'name' => 'a5', 'mm_size' => {'width' => '35', 'height' => '21'} },
+                             { 'name' => '8464', 'mm_size' => {'width' => '84', 'height' => '64'} },
+                             { 'name' => 'custom', 'mm_size' => {'width' => '84', 'height' => '64'} }]
 
       setting :label_variables, [{ 'varname' => 'Commodity',
                                    'test_value' => 'PLUM' },
                                  { 'varname' => 'Variety',
                                    'test_value' => 'Golden Delicious' }]
+
+      setting :label_config,  {
+                                labelState: 'new', # new, edit, copy, preview
+                                labelName: 'xxx',
+                                labelJSON: {},
+                                savePath: '/xyz'
+                              }
     end
   end
 end
